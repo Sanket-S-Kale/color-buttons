ae53dc66847ae987313da8d5c643cd8980ce5bd8
Sanket Kale <sanket9k@gmail.com>
2022-03-01 15:35:22 -0600

    Guided tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+  expect(button).toHaveStyle({ backgroundColor: 'red' });
+
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: 'blue'});
+  
+  expect(button.textContent).toBe('Change to red');
+});
+
+test('initial conditions', () => {
+  render(<App />);
+
+  const button = screen.getByRole('button', { name: 'Change to blue'});
+  expect(button).toBeEnabled();
+
+  const checkBox = screen.getByRole('checkbox');
+  expect(checkBox).not.toBeChecked();
 });
